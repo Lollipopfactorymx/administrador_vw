ffd55c514c5b918b2e6292fc750e45f67ca8badb
leonardo <leodanlors1993@gmail.com>
2021-06-02 02:00:22 -0500

    first mockup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,81 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <v-app>
+      <v-card
+        class="mx-auto overflow-hidden"
+        height="100vh"
+        width="100%"
+      >
+        <v-app-bar
+          color="light-blue darken-2"
+          dark
+        >
+          <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    
+          <v-toolbar-title>Administrador</v-toolbar-title>
+        </v-app-bar>
+        <router-view></router-view>
+        <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary
+        >
+        <v-col>
+          <v-avatar color="indigo">
+            <v-icon dark>
+              mdi-account-circle
+            </v-icon>
+          </v-avatar>
+            <p>Usuario</p>
+        </v-col>  
+        
+          <v-list
+            nav
+            dense
+          >
+            <v-list-item-group
+              v-model="group"
+              active-class="blue darken-3--text text--accent-4"
+            >
+             <router-link to="/dashboard">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                  <v-list-item-title>Dashboard</v-list-item-title>
+              </v-list-item>
+              </router-link>
+              <router-link to="/login">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-account</v-icon>
+                </v-list-item-icon>
+                
+                  <v-list-item-title>Login</v-list-item-title>
+                
+              </v-list-item>
+              </router-link>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
+      
+    </v-app>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      group: null,
     }
-  }
+  },
+   watch: {
+    group () {
+      this.drawer = false
+    },
+  },
+
 }
-</style>
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-row dense justify="center" align="center">
+      <v-col cols="12"
+        md="8"
+        height="100px"
+      >
+      <v-card
+        color="#385F73"
+        dark
+      >
+        <v-card-title class="text-h5">
+          Lorem ipsum 
+        </v-card-title>
+        <v-card-subtitle>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Facilis sed, delectus assumenda exercitationem unde quae inventore excepturi iure quibusdam. 
+          Ad quis unde minus dicta tempora ex at culpa totam incidunt.
+        </v-card-subtitle>
+        <v-card-actions>
+        <v-btn text>
+          Lorem ipsum
+        </v-btn>
+        </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>  
+      <v-col cols="12"
+        md="8"
+      >
+      <v-card
+        color="#385F73"
+        dark
+      >
+        <v-card-title class="text-h5">
+          Lorem ipsum 
+        </v-card-title>
+        <v-card-subtitle>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Facilis sed, delectus assumenda exercitationem unde quae inventore excepturi iure quibusdam. 
+          Ad quis unde minus dicta tempora ex at culpa totam incidunt.
+        </v-card-subtitle>
+        <v-card-actions>
+        <v-btn text>
+          Lorem ipsum
+        </v-btn>
+        </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Dashboard"
+};
+</script>
